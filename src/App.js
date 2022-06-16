e7844997bc0d7ca210e8a62c083ef6186fc3283e
Darman1987 <darman1987@hotmail.com>
2022-06-16 14:12:15 +1000

    components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import './App.css';
+import Navbar from './Navbar'; /*This is the way to import components */
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const colors = ['blue','yellow','red'];
- const person = { name: 'diego', age: 35 };
-  const link = 'http://www.google.com';
-
   return (
-    
     <div className="App">
-     {/* only can have one root element in JSX <div className="App">*/}
-    {/* Insted of use attribute class we should use className*/}
+      <Navbar /> {/*This is the way to call components */}
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ person.name }</p> 
-         
-        <p>{ 10 }</p>
-        <p>{ colors }</p>
-        <p>{ "hello, friends" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );

e89a616174f19c61fe39ff8196076e88386e5302
Darman1987 <darman1987@hotmail.com>
2022-06-17 11:35:31 +1000

    events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
-    return (
-      <div className="home">
-        <h2>Homepage</h2>
-      </div>
-    );
+
+  const handleClick = (e) => {
+    console.log('event :', e);
   }
-   
-  export default Home;/*This is the way to export components */
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
+  }
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+    </div>
+  );
+}
+ 
+export default Home;

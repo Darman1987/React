f50543057846a6fcb4a0b87b5d8d91fd9d9c1914
Darman1987 <darman1987@hotmail.com>
2022-06-19 20:15:19 +1000

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,12 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'diego', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'armando', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'diego', id: 3 }
   ])
+
+  const [name, setName] = useState('Charl');
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,14 +16,16 @@ const Home = () => {
   }
 
   //By default, it runs both after the first render and after every update.
-  useEffect(() => {   
+  useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
+    console.log(name);
   })
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Chris')}>change name</button>
     </div>
   );
 }

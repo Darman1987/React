6ab9ae44ca9d03b2033bbfa2ba82c0c57a2fe437
Darman1987 <darman1987@hotmail.com>
2022-06-19 21:00:04 +1000

    Fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,21 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'diego', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'armando', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'diego', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null)
 
-  const [name, setName] = useState('Charl');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  //By default, it runs both after the first render and after every update.
   useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-    console.log(name);
-  })
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('Chris')}>change name</button>
+      {blogs && <BlogList blogs={blogs} />} {/*{blogs && } only create components when variable blogs exist */}
     </div>
   );
 }

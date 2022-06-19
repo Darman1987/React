527252d07713119faaaa4e5d31c803d91ded138d
Darman1987 <darman1987@hotmail.com>
2022-06-19 20:03:01 +1000

    useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'diego', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'armando', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'diego', id: 3 }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
+
+  //By default, it runs both after the first render and after every update.
+  useEffect(() => {   
+    console.log('use effect ran');
+    console.log(blogs);
+  })
 
   return (
     <div className="home">

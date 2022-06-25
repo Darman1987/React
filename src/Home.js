4c6d277ea5337106455bd1c335b9b00460157c2e
Darman1987 <darman1987@hotmail.com>
2022-06-25 19:59:00 +1000

    Loading message

--- src/Home.js
+++ src/Home.js
@@ -2,21 +2,26 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+  useEffect(() => {  
+    setTimeout(() => {   //The function setTimeout was used to simulate load time
+      fetch('http://localhost:8000/blogs')
       .then(res => {
         return res.json();
       })
       .then(data => {
+        setIsPending(false);
         setBlogs(data);
       })
+    }, 1000);
   }, [])
 
   return (
     <div className="home">
-      {blogs && <BlogList blogs={blogs} />} {/*{blogs && } only create components when variable blogs exist */}
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 }
